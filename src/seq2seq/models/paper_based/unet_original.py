7c4a641597ac459ce244a481f303b3b85fe3cdd8
Guillermo Kulemeyer <gkulemeyer@sinc.unl.edu.ar>
2025-03-25 17:15:26 -0300

    add first exp with cuda !

--- src/seq2seq/models/paper_based/unet_original.py
+++ src/seq2seq/models/paper_based/unet_original.py
@@ -90,13 +90,13 @@ class Seq2Seq(nn.Module):
         **kwargs
     ):      
         
-        # features = [4]
-        # n_4=2
-        # n_8=1
-        # for _ in range(n_4):
-        #     features.append(4)
-        # for _ in range(n_8):
-        #     features.append(8)
+        features = [4]
+        n_4=3
+        n_8=3
+        for _ in range(n_4):
+            features.append(4)
+        for _ in range(n_8):
+            features.append(8)
  
         
         rev_features = features[::-1]
